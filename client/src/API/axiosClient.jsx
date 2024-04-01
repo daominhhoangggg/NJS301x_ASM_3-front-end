dfab9f708ed30ce1e6f65fb4523101cefe42b7ff
Minh Hoang <daominhhoangggg@gmail.com>
2024-04-01 15:04:36 +0700

    client update and deploy

--- client/src/API/axiosClient.jsx
+++ client/src/API/axiosClient.jsx
@@ -4,7 +4,7 @@ import queryString from "query-string";
 // Set up default config for http requests here
 // Please have a look at here `https://github.com/axios/axios#requestconfig` for the full list of configs
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://njs301x-asm-3-back-end.onrender.com",
   headers: {
     "content-type": "application/json",
   },
